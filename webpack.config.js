b4c9a37083025d01326d6e09ffcbe8b60d94b082
richa.goel <richa@repozitory.com>
2021-02-27 18:47:50 +0530

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+module.exports = function (env) {
+    console.log("env = ", env);
+    return {
+        target: 'node',
+        externals: [nodeExternals()],
+        entry: {
+            'index': './app.js',
+        },
+        output: {
+            path: path.join(__dirname, 'build'),
+            filename: '[name].bundle.js',
+            libraryTarget: 'commonjs2'
+        }
+    }
+}
